afcf5ddcb1869e6d71f19b7d53d811b3793a7a8e
udaygoshike2 <udaygoshike2@gmail.com>
2025-12-15 10:49:04 +0000

    second change done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ts-cs/',   // <<< IMPORTANT 100% REQUIRED
 })
